e33f75a687e5c7cdbf5c1d309c7acd22d89b7000
Justin Wang <jjwang01@stanford.edu>
2019-04-18 20:58:38 -0700

    Can't deal with variable number of columns. Used regex to deal with different delimiters.

---
+++ parse_gtf.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pandas as pd
+
+# Input
+data_file = "gencode.vM7.annotation.gtf"
+
+# Delimiter
+data_file_delimiter = '\t|;'
+
+# The max column count a line in the file could have
+largest_column_count = 0
+
+# Loop the data lines
+with open(data_file, 'r') as temp_f:
+    # Read the lines
+    lines = temp_f.readlines()
+
+    for l in lines:
+        # Count the column count for the current line
+        column_count = len(l.split(data_file_delimiter)) + 1
+
+        # Set the new most column count
+        largest_column_count = column_count if largest_column_count < column_count else largest_column_count
+
+# Close file
+temp_f.close()
+
+column_names = [i for i in range(0, largest_column_count)]
+
+gtf = pd.read_csv(
+    filepath_or_buffer= data_file,
+    sep=data_file_delimiter,
+    header=None,
+    names=column_names,
+    skiprows=[i for i in range(25)])
+
+gtf = gtf[gtf['1'].notnull()]
+filtered_gtf = gtf[gtf['2'] == 'gene']
+filtered_2_gtf = filtered_gtf[filtered_gtf['9'] == 'gene_type "protein_coding"']
+
+print(filtered_2_gtf.head())
+"""
+# loop through all rows
+for index, row in gtf.iterrows():
+	columns = row['attributes'].str.split(';', expand=True)
+	gtf.iloc[index] = 
+
+
+
+gtf['gene_id', 
+	'transcript_id', 
+	'gene_type', 
+	'others'
+	] = gtf['attributes'].str.split(';',expand=True)
+"""
+
+print(filtered_gtf.head())
+
