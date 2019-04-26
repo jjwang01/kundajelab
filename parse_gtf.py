6058c4b21cc4ea0b6f441beaeefdf3fd1313d6c8
Justin Wang <jjwang01@stanford.edu>
2019-04-26 02:00:07 -0700

    Finished parsing gene matrix.

--- parse_gtf.py
+++ parse_gtf.py
@@ -1,73 +1,62 @@
+import os
+import gzip
 import pandas as pd
 import re
 
-fo = open('gencode.vM7.annotation.gtf', 'r')
-f = fo.readlines()
-fo.close()
+# predefine file locations, folders
+out_dir = "/."
+file_name = "gencode.vM7.annotation.gtf.gz"
+file_url = "ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M7/gencode.vM7.annotation.gtf.gz"
+
+# run code here
+download_cmd = " wget {} -O {}/{}".format(file_url, out_dir, file_name)
+
+try:
+	gtf = open("{}/{}".format(out_dir, file_name))
+	print('found')
+except FileNotFoundError:
+	print('not found')
+	os.system(download_cmd)
+
+f = []
+with gzip.open("{}/{}".format(out_dir, file_name)) as f_unz:
+	f = f_unz.readlines()
+	f_unz.close()
+
 f = f[25:]
 f = [line.strip() for line in f]
+f = [line.decode("utf-8") for line in f]
 gtf = pd.DataFrame(re.split(r'\t|;', line) for line in f)
 
-#gtf = gtf[gtf[1].notnull()]
 filtered_gtf = gtf[gtf[2] == 'gene']
-filtered_2_gtf = filtered_gtf[filtered_gtf[9] == ' gene_type \"protein_coding\"']
-
-print(filtered_2_gtf.head())
-"""
-# Input
-data_file = "gencode.vM7.annotation.gtf"
-
-# Delimiter
-data_file_delimiter = '\t|;'
-
-# The max column count a line in the file could have
-largest_column_count = 0
-
-# Loop the data lines
-with open(data_file, 'r') as temp_f:
-    # Read the lines
-    lines = temp_f.readlines()
-
-    for l in lines:
-        # Count the column count for the current line
-        column_count = len(l.split(data_file_delimiter)) + 1
-
-        # Set the new most column count
-        largest_column_count = column_count if largest_column_count < column_count else largest_column_count
-
-# Close file
-temp_f.close()
-
-column_names = [i for i in range(0, largest_column_count)]
-
-gtf = pd.read_csv(
-    filepath_or_buffer= data_file,
-    sep=data_file_delimiter,
-    header=None,
-    names=column_names,
-    skiprows=[i for i in range(25)],
-    engine=python)
-
-gtf = gtf[gtf['1'].notnull()]
-filtered_gtf = gtf[gtf['2'] == 'gene']
-filtered_2_gtf = filtered_gtf[filtered_gtf['9'] == 'gene_type "protein_coding"']
-
-print(filtered_2_gtf.head())
-"""
-"""
-# loop through all rows
-for index, row in gtf.iterrows():
-	columns = row['attributes'].str.split(';', expand=True)
-	gtf.iloc[index] = 
-
-
-
-gtf['gene_id', 
-	'transcript_id', 
-	'gene_type', 
-	'others'
-	] = gtf['attributes'].str.split(';',expand=True)
-
-
+filtered_gtf = filtered_gtf[filtered_gtf[9] == ' gene_type \"protein_coding\"']
 print(filtered_gtf.head())
-"""
+
+# import the gene matrix file
+gene_matrix_name = "GSE109125_Gene_count_table.csv"
+gene_matrix_url = "https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE109125"
+
+# run code here
+download_cmd = " wget {} -O {}/{}".format(gene_matrix_url, out_dir, gene_matrix_name)
+
+try:
+	gtf = open("{}/{}".format(out_dir, gene_matrix_name))
+	print('found')
+except FileNotFoundError:
+	print('not found')
+	os.system(download_cmd)
+
+# header is implicitly the first line of the file
+gene_matrix = pd.read_csv("{}/{}".format(out_dir, gene_matrix_name))
+print(gene_matrix.head())
+
+gene_matrix_names = set(gene_matrix["Gene_Symbol"].tolist())
+gtf_names = []
+# gene_names are formatted like "gene_name "...""
+for value in filtered_gtf[11].tolist():
+    gtf_names.append(value[12:-1])
+gtf_names = set(gtf_names)
+
+intersect = set.intersection(gene_matrix_names, gtf_names)
+filtered_gene_matrix = gene_matrix[gene_matrix["Gene_Symbol"].isin(intersect)]
+print(filtered_gene_matrix.head())
